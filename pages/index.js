be1d03fcc4de3dec44b2207fb3e419b501a62219
nmegrant <nme.grant@gmail.com>
2020-12-11 17:45:44 +0100

    Set up book page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ books }) {
+  console.log(books);
   return (
     <div className={styles.container}>
       <Head>
@@ -10,14 +12,16 @@ export default function Home({ books }) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to my book!</h1>
+        <h1 className={styles.title}>Welcome to my book site!</h1>
 
         <div className={styles.grid}>
           {books.items.map((book) => (
-            <div className={styles.card} key={book.id}>
-              <h3>{book.volumeInfo.title}</h3>
-              <p>{book.volumeInfo.description}</p>
-            </div>
+            <Link key={book.id} href={`/books/${book.id}`}>
+              <div className={styles.card}>
+                <h3>{book.volumeInfo.title}</h3>
+                <p>{book.volumeInfo.description}</p>
+              </div>
+            </Link>
           ))}
         </div>
       </main>

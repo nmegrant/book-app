10d28d6862ef044afd51b1bdb6efb9dc2c43d2d4
nmegrant <nme.grant@gmail.com>
2021-02-13 22:44:35 +0100

    Basic styling of navbar

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,27 @@
 import Link from "next/link";
+import styles from "./layout.module.css";
 
 export default function Layout({ children }) {
   return (
     <div>
-      <div>
+      <div className={styles.header}>
         <Link href="/">
-          <a>Main Page</a>
+          <a className={styles.navItem}>Main Page</a>
         </Link>
         <Link href="/browse">
-          <a>Browse</a>
+          <a className={styles.navItem}>Browse</a>
         </Link>
         <Link href="/checkout">
-          <a>Checkout</a>
+          <a className={styles.navItem}>Checkout</a>
         </Link>
       </div>
       <div>{children}</div>
     </div>
   );
 }
+
+// 239, 189, 235 Pink lavender
+// 182, 140, 184 African violet
+// 100, 97, 160 Dark blue grey
+// 49, 76, 182 Violet blue
+// 10, 15, 13 Rich black

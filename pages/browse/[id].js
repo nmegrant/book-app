a0080966c719c538fe4361d25e8c3b024a253288
nmegrant <nme.grant@gmail.com>
2021-03-16 22:23:59 +0100

    Set up router to get book id

--- pages/browse/[id].js
+++ pages/browse/[id].js
@@ -1,12 +1,16 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import styles from "../../styles/home.module.css";
 
 export default function Browse() {
+  const router = useRouter();
+  const {
+    query: { id },
+  } = router;
   return (
     <div className={styles.container}>
       <Head>
         <title>Title</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Book Title</h1>

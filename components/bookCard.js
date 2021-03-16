1a29b263c2e49e5195a05bbd57120de4d31d4ca9
nmegrant <nme.grant@gmail.com>
2021-03-16 22:14:24 +0100

    Set up for book page

--- components/bookCard.js
+++ components/bookCard.js
@@ -1,6 +1,8 @@
 import styles from "../styles/home.module.css";
+import Link from "next/link";
 
 export default function BookCard({
+  id,
   title,
   author,
   description,
@@ -9,13 +11,15 @@ export default function BookCard({
   rating,
 }) {
   return (
-    <div className={styles.card}>
-      <h1>{title}</h1>
-      <h5>by {author}</h5>
-      <p>{description}</p>
-      <h5>price: ${price}</h5>
-      <h5>stock: {stock}</h5>
-      <h5>rating: {rating}/5</h5>
-    </div>
+    <Link href={`/browse/${id}`}>
+      <div className={styles.card}>
+        <h1>{title}</h1>
+        <h5>by {author}</h5>
+        <p>{description}</p>
+        <h5>price: ${price}</h5>
+        <h5>stock: {stock}</h5>
+        <h5>rating: {rating}/5</h5>
+      </div>
+    </Link>
   );
 }

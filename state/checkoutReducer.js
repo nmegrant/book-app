07b5f13795f472c504eb5c2d875c464af0ffc283
nmegrant <nme.grant@gmail.com>
2021-03-21 20:51:30 +0100

    Set up checkout reducer and context

---
+++ state/checkoutReducer.js
@@ -0,0 +1,12 @@
+export const initialState = ["test"];
+
+export function checkoutReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.payload];
+    case "REMOVE":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}

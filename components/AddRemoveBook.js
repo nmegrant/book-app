ec5369482481f16b26fdbe39c5f5818d1def9c9c
nmegrant <nme.grant@gmail.com>
2021-03-22 22:22:45 +0100

    Improve button styling

--- components/AddRemoveBook.js
+++ components/AddRemoveBook.js
@@ -1,15 +1,22 @@
 import React, { useContext } from "react";
 import { CheckOutContext } from "../state/CheckOutContext";
+import styles from "../styles/home.module.css";
 
 export default function AddRemoveBook({ book }) {
   const { state, dispatch } = useContext(CheckOutContext);
 
   return (
     <div>
-      <button onClick={() => dispatch({ type: "ADD", payload: book })}>
+      <button
+        onClick={() => dispatch({ type: "ADD", payload: book })}
+        className={styles.counterbutton}
+      >
         Add Book
       </button>
-      <button onClick={() => dispatch({ type: "REMOVE", payload: book })}>
+      <button
+        onClick={() => dispatch({ type: "REMOVE", payload: book })}
+        className={styles.counterbutton}
+      >
         Remove Book
       </button>
     </div>
